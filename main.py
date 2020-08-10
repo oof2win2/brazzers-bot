25b809fb4e42191bdba7cb3229a9f826c7de2bf3
oof2win2 <honza.kocourek.parklane@gmail.com>
2020-08-10 22:07:23 +0200

    Added a second input for verification purposes. Renamed test.py to main.py because the program worked as it was supposed to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-import time
+# This file is here just to test things using the different libraries so it isn't in main.py
+# Shouldn't be in final release (idk)
+
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
@@ -9,23 +10,15 @@ img = Image.open('input.jpg').convert('RGBA')
 
 # outputs the size of the image
 width, height = img.size
-#print('width is: ', width)
-#print('height is:', height)
+print('original width, height: ', width, height)
 
 # creates the watermark as a new image, as if just overlaying one image with the other
-# watermark = Image.new('watermark.png', (width, height), color=(255, 255, 255, 0))
+watermark = Image.open('watermarks/brazzers.png').convert('RGBA')
+watermark = watermark.resize((round(width/3.375), round(height/6))) ##resizes watermark to make it smaller and make it be in a 16:9 aspect ratio
+w, h = watermark.size
 
-fnt = ImageFont.truetype('C:\Windows\Fonts\Arial.ttf', 192)
-a = ImageDraw.Draw(img)
-a.text((10, 10), "Hello World", font=fnt, fill = (63, 63, 63))
+print('overlay width, height', w, h)
 
+img.paste(watermark, (width - w - round(width/50), height - h), watermark) #pasting the watermark onto the import image
 img.save('out.png')
-"""
-# output process
-img.paste(watermark, (0, 0), watermark)
-watermark.close()
-#display the output image
 img.show()
-img.save('out.png')
-img.close()
-"""
